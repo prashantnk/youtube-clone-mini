4def6bd420e753e702c6589d016a768ffeb88685
prashant <prashantnk2001@gmail.com>
2021-11-17 15:54:01 +0530

    add videoplayer and complete application

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 
 class App extends React.Component {
 
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onFormSubmit = async (data) => {
 
@@ -17,14 +18,31 @@ class App extends React.Component {
             }
         };
         const result = await youtube.get(endPoint, options);
-        this.setState({ videos: result.data.items })
+        this.setState({ videos: result.data.items, selectedVideo: result.data.items[0] })
     }
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    }
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onFormSubmit} />
-                <h3>Showing {this.state.videos.length} videos </h3>
-                <VideoList videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="row">
+                        <div className="eleven wide column">
+                            <h3>Video Player</h3>
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <h3>Showing {this.state.videos.length} videos </h3>
+                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         );
     }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from "./VideoItem";
 
 const VideoList = (props) => {
     const videos = props.videos.map((video) => {
-        return <VideoItem video={video} key={video.id.videoId} />;
+        return <VideoItem video={video} key={video.id.videoId} onVideoSelect={props.onVideoSelect} />;
     })
     return (
         <div className="ui list">
